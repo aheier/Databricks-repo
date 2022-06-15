2463e8faa0c09d4aabfdf37dc434154f86d72441
Anthony Heier <aheier@fastenal.com>
2022-06-15 10:06:27 -0500

    config

--- Test.py
+++ Test.py
@@ -1,11 +1,15 @@
+"""
+This is a test python script
+"""
+
+
 # Databricks notebook source
 print('hello world')
-
 # COMMAND ----------
 
 # DBTITLE 1,Some title here
-x = 2 + 2
-print(x)
+TEMP_VAR = 2 + 2
+print(TEMP_VAR)
 
 # COMMAND ----------
 
@@ -14,3 +18,13 @@ print(x)
 # COMMAND ----------
 
 #asdfasdf
+
+def some_func(input_str: str):
+    """
+    prints the inputted string
+    """
+    print(f"this is a func: {input_str}")
+
+
+if __name__ == '__main__':
+    some_func("hello world")
